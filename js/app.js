1ed438f0e2f1aeb849e77fadfd71f3a2b742ae8d
Nahuel de la Vega <nahueldlvega@gmail.com>
2022-04-20 15:15:20 -0300

    challenge 12 listo

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Area del triangulo
+
+let base = 20;
+let height = 10;
+
+
+let triangleArea = (base*height/2);
+console.log(triangleArea);
+
+// Area del circulo
+
+let circleRadio = 50;
+
+let circleArea = (Math.PI*(Math.pow(circleRadio,2)))
+console.log (Math.round(circleArea));
